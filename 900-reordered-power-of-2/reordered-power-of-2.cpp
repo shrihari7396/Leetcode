89c9a296c83576a1815c954e101351c2b3635c6e
SHRIHARI KULKARNI <163467964+shrihari7396@users.noreply.github.com>
2025-08-10 15:46:29 +0530

    Time: 138 ms (5.60%) | Memory: 9.2 MB (34.22%) - LeetSync

--- 900-reordered-power-of-2/reordered-power-of-2.cpp
+++ 900-reordered-power-of-2/reordered-power-of-2.cpp
@@ -1,7 +1,12 @@
 class Solution {
 private:
     bool isPowerOfTwo(int n) {
-        return n > 0 && (n & (n - 1)) == 0;
+        int cnt = 0;
+        while(n > 0) {
+            if(n&1) cnt ++;
+            n = n >> 1;
+        }
+        return cnt == 1;
     }
 
     string vecToS(vector<char>& ch) {
@@ -22,7 +27,7 @@ public:
 
         sort(ch.begin(), ch.end());
         do {
-            if (ch[0] == '0') continue;
+            if(ch[0] == '0') continue;
             int num = stoi(vecToS(ch));
             if(isPowerOfTwo(num)) return true;
         } while(next_permutation(ch.begin(), ch.end()));
