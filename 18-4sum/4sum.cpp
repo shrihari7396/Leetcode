a0c9171a5ba803aa0c1bccd361f30ef3c0a19c44
SHRIHARI KULKARNI <163467964+shrihari7396@users.noreply.github.com>
2025-08-13 16:59:31 +0530

    Time: 19 ms (81.78%) | Memory: 17.5 MB (28.26%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -1,26 +1,39 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        vector<vector<int>> ans;
         int n = nums.size();
-        set<vector<int>> ansTemp;
+        sort(nums.begin(), nums.end());
 
-        for(int i = 0; i < n - 2; i++) {
-            for(int j = i + 1; j < n - 1; j++) {
-                unordered_set<long long> hash;
-                for(int k = j + 1; k < n; k++) {
-                    long long sum = (long long)nums[i] + nums[j] + nums[k];
-                    long long required = (long long)target - sum;
+        for(int i = 0; i < n; i++) {
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            for(int j = i + 1; j < n; j++) {
+                if(j > i+1 && nums[j] == nums[j-1]) continue;
 
-                    if(hash.find(required) != hash.end()) {
-                        vector<int> tmp = {nums[i], nums[j], nums[k], (int)required};
-                        sort(tmp.begin(), tmp.end());
-                        ansTemp.insert(tmp);
+                int k = j + 1, l = n-1;
+
+                while(k < l) {
+                    long long sum = (long long)nums[i] + nums[j] + nums[k] + nums[l];
+
+                    if(sum > target) {
+                        l--;
+                    } else if(sum < target) {
+                        k++;
+                    } else {
+                        vector<int> tmp = {nums[i], nums[j], nums[k], nums[l]};
+                        ans.emplace_back(tmp);
+                        k++;
+                        l--;
+                        while(k < l && nums[k] == nums[k-1]) {
+                            k++;
+                        }
+                        while(k < l && nums[l] == nums[l+1]) {
+                            l--;
+                        }
                     }
-
-                    hash.insert(nums[k]);
                 }
             }
         }
-        return vector<vector<int>>(ansTemp.begin(), ansTemp.end());
+        return ans;
     }
 };
