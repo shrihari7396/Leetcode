73854cb29fd28c420e056c12bf6423806f30ff81
SHRIHARI KULKARNI <163467964+shrihari7396@users.noreply.github.com>
2025-08-14 09:37:31 +0530

    Time: 6 ms (3.26%) | Memory: 9.8 MB (9.90%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.cpp
@@ -3,7 +3,7 @@ public:
     string largestGoodInteger(string s) {
         unordered_map<char, int> m;
         int i = 0, j = 0;
-        vector<int> ans;
+        int best = -1;
         while(j < s.size()) {
             m[s[j]] ++;
 
@@ -17,17 +17,12 @@ public:
 
             if(m.size() == 1 && m[s[i]] == 3) {
                 int number = s[i] - '0';
-                ans.emplace_back(number);
+                if(number > best) best = number;
             }
             j++;
         }
-        if(ans.empty()) return "";
+        if(best == -1) return "";
 
-        int element = *max_element(ans.begin(), ans.end());
-        string finalAns = "";
-        for(int i = 0; i < 3; i++) {
-            finalAns = finalAns + to_string(element);
-        }
-        return finalAns;
+        return string(3, '0' + best);
     }
 };
