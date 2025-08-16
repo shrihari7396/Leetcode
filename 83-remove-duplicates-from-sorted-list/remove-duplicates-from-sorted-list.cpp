2239bba36a57cde39c9bb40d0f08db5d20393da5
SHRIHARI KULKARNI <163467964+shrihari7396@users.noreply.github.com>
2025-08-16 23:45:12 +0530

    Time: 0 ms (100.00%) | Memory: 16 MB (99.31%) - LeetSync

--- 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.cpp
+++ 83-remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.cpp
@@ -1,31 +1,17 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        int prev = INT_MIN;
-        ListNode* dummy = new ListNode();
-        ListNode* curr = dummy;
-        while(head && head -> next) {
-            if(prev != head -> val) {
-                ListNode* newNode = new ListNode(head -> val);
-                curr -> next = newNode;
-                curr = curr -> next;
-                prev = head -> val;
+        if (!head) return nullptr;
+
+        ListNode* curr = head;
+        while (curr && curr->next) {
+            if (curr->val == curr->next->val) {
+                // skip duplicate node
+                curr->next = curr->next->next;
+            } else {
+                curr = curr->next;
             }
-            head = head -> next;
         }
-        if(head && prev != head -> val) {
-            curr -> next = new ListNode(head->val);
-        }
-        return dummy -> next;
+        return head;
     }
 };
