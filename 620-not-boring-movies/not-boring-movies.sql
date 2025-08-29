4bb54aaacda744816af65db78cab3adcec47e032
SHRIHARI KULKARNI <163467964+shrihari7396@users.noreply.github.com>
2025-08-30 00:53:16 +0530

    Time: 947 ms (5.00%) | Memory: 0B (100.00%) - LeetSync

---
+++ 620-not-boring-movies/not-boring-movies.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+SELECT 
+    id,
+    movie,
+    description,
+    rating
+FROM Cinema
+WHERE description <> 'boring' AND id % 2 = 1
+ORDER BY rating DESC;
