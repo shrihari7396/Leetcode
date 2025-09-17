c0d8b6c255a64669958e5139a9f0b54b0d593728
SHRIHARI KULKARNI <163467964+shrihari7396@users.noreply.github.com>
2025-09-17 15:05:02 +0530

    Time: 130 ms (72.30%) | Memory: 162.6 MB (89.75%) - LeetSync

--- 2429-design-a-food-rating-system/design-a-food-rating-system.cpp
+++ 2429-design-a-food-rating-system/design-a-food-rating-system.cpp
@@ -3,10 +3,7 @@ using namespace std;
 
 class FoodRatings {
 private:
-    // food -> {cuisine, rating}
     unordered_map<string, pair<string, int>> foodInfo;
-
-    // cuisine -> ordered set of (rating, food)
     unordered_map<string, set<pair<int, string>>> cuisineFoods;
 
 public:
@@ -14,26 +11,18 @@ public:
         int n = foods.size();
         for (int i = 0; i < n; i++) {
             foodInfo[foods[i]] = {cuisines[i], ratings[i]};
-            // store as (-rating, food) so set sorts highest rating first
             cuisineFoods[cuisines[i]].insert({-ratings[i], foods[i]});
         }
     }
     
     void changeRating(string food, int newRating) {
-        auto [cuisine, oldRating] = foodInfo[food];
-        
-        // remove old entry
+        auto [cuisine, oldRating] = foodInfo[food];    
         cuisineFoods[cuisine].erase({-oldRating, food});
-        
-        // update rating
         foodInfo[food].second = newRating;
-        
-        // insert new entry
         cuisineFoods[cuisine].insert({-newRating, food});
     }
     
     string highestRated(string cuisine) {
-        // first element of set is always the highest-rated
         return cuisineFoods[cuisine].begin()->second;
     }
 };
