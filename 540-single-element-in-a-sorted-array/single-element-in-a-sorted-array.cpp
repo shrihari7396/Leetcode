0a0ab990791806ff4955a9a5e00edc5f9aaab8d7
SHRIHARI KULKARNI <163467964+shrihari7396@users.noreply.github.com>
2025-09-25 15:24:29 +0530

    Time: 0 ms (100.00%) | Memory: 26.3 MB (33.83%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -3,7 +3,6 @@ public:
     int singleNonDuplicate(vector<int>& nums) {
         int xor1 = 0;
         for(int i = 0; i < nums.size(); i++) xor1 ^= nums[i];
-        return xor1
-        ;
+        return xor1;
     }
 };
