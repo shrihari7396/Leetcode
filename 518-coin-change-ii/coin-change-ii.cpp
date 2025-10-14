4747ca5f0bbb06ad447e14a88551913a59d05de0
SHRIHARI KULKARNI <163467964+shrihari7396@users.noreply.github.com>
2025-10-14 14:01:44 +0530

    Time: 1172 ms (5.00%) | Memory: 132.2 MB (5.48%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.cpp
+++ 518-coin-change-ii/coin-change-ii.cpp
@@ -1,13 +1,15 @@
 class Solution {
 private:
     int helper(int i, int amount, vector<int>& coins, map<pair<int, int>, int>& m) {
-        if(amount < 0) return 0;
-        if(i < 0) return 0;
-        if(i == 0 && amount == 0) return 1;
+        if (amount == 0) return 1;
+        if (i == 0) return (amount % coins[0] == 0);
+
         if(m.find({i, amount}) != m.end()) {
             return m[{i, amount}];
         }
-        int take = helper(i, amount-coins[i], coins, m);
+        int take = 0;
+        if (amount >= coins[i]) 
+            take = helper(i, amount - coins[i], coins, m);
         int notTake = helper(i-1, amount, coins, m);
         return m[{i, amount}] = take + notTake;
     }
