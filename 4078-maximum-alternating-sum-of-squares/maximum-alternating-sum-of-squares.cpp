a5d9d1c2e613bb14889f211166b7ff4c44ff4d6a
SHRIHARI KULKARNI <163467964+shrihari7396@users.noreply.github.com>
2025-10-26 12:08:27 +0530

    Time: 61 ms (54.55%) | Memory: 175.9 MB (9.09%) - LeetSync

---
+++ 4078-maximum-alternating-sum-of-squares/maximum-alternating-sum-of-squares.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    long long maxAlternatingSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end(), [](int a, int b) {
+            return abs(a) < abs(b);
+        });
+
+        int i = 0, j = nums.size() - 1;
+        long long sum = 0;
+        while (i <= j) {
+            sum += 1LL * nums[j] * nums[j];
+            j--;
+            if (i <= j) {
+                sum -= 1LL * nums[i] * nums[i];
+                i++;
+            }
+        }
+        return sum;
+    }
+};
