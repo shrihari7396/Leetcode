a999b5414a636ec208382dbb8efb904083137d29
SHRIHARI KULKARNI <163467964+shrihari7396@users.noreply.github.com>
2025-11-01 16:16:51 +0530

    Time: 114 ms (51.98%) | Memory: 131.2 MB (48.45%) - LeetSync

---
+++ 1492-time-needed-to-inform-all-employees/time-needed-to-inform-all-employees.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+        vector<vector<int>> adj(n);
+
+        for(int i = 0; i < n; i++) {
+            int u = manager[i];
+            int v = i;
+            if(u == -1) continue;
+            int time = informTime[u];
+            adj[u].emplace_back(v);
+        }
+
+        queue<pair<int, int>> q;
+        q.push({headID, 0});
+        int timeRequired = 0;
+        while(!q.empty()) {
+            auto [node, currTime] = q.front();
+            q.pop();
+            timeRequired = max(timeRequired, currTime);
+            for (int sub : adj[node]) {
+                q.push({sub, currTime + informTime[node]});
+            }
+        }
+        return timeRequired;
+    }
+};
