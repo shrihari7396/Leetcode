7478dda9b49fb5d2955b351d89955640c4518947
SHRIHARI KULKARNI <163467964+shrihari7396@users.noreply.github.com>
2025-11-03 17:46:03 +0530

    Time: 36 ms (25.46%) | Memory: 27.1 MB (45.37%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,27 +1,24 @@
-#include <bits/stdc++.h>
-using namespace std;
 
 class Solution {
 private:
-    int dfs(int i, int j, string& s, string& t, vector<vector<int>>& dp) {
-        if (j == t.size()) return 1;      // t completely matched
-        if (i == s.size()) return 0;      // s finished but t not done
-        if (dp[i][j] != -1) return dp[i][j];
+    int dfs(int i, int j, int n, int m, string& s, string& t, vector<vector<int>>& dp) {
+        if(j == m) {
+            return 1;
+        } else if(i == n) {
+            return 0;
+        } else if(dp[i][j] != -1) return dp[i][j];
 
-        // Option 1: skip s[i]
-        int ways = dfs(i + 1, j, s, t, dp);
-
-        // Option 2: take s[i] if it matches t[j]
-        if (s[i] == t[j])
-            ways += dfs(i + 1, j + 1, s, t, dp);
-
+        int ways = dfs(i+1, j, n, m, s, t, dp);
+        if(s[i] == t[j]) {
+            ways += dfs(i+1, j+1, n, m, s, t, dp);
+        }
         return dp[i][j] = ways;
     }
-
 public:
     int numDistinct(string s, string t) {
-        int n = s.size(), m = t.size();
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return dfs(0, 0, s, t, dp);
+        int m = t.size();    
+        int n = s.size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        return dfs(0,0,n,m,s,t,dp);
     }
 };
