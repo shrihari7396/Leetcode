ded36a9b76a56a9ee3112a57c48bc709f27c3d96
SHRIHARI KULKARNI <163467964+shrihari7396@users.noreply.github.com>
2025-11-03 21:31:48 +0530

    Time: 5 ms (42.91%) | Memory: 100 MB (17.95%) - LeetSync

--- 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.cpp
+++ 1700-minimum-time-to-make-rope-colorful/minimum-time-to-make-rope-colorful.cpp
@@ -1,49 +1,17 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
-private:
-    struct Pair {
-        int first, second;
-        Pair(int f, int s) : first(f), second(s) {}
-    };
-
-    int process(const Pair& p, const vector<int>& prefixSum, const vector<int>& neededTime) {
-        int sum = prefixSum[p.second + 1] - prefixSum[p.first];
-        int maxVal = 0;
-        for (int i = p.first; i <= p.second; i++) {
-            maxVal = max(maxVal, neededTime[i]);
-        }
-        return sum - maxVal;
-    }
-
 public:
     int minCost(string colors, vector<int>& neededTime) {
-        int n = colors.size();
-        vector<Pair> subarrays;
-
-        int i = 0;
-        while (i < n) {
-            int j = i;
-            while (j + 1 < n && colors[i] == colors[j + 1]) {
-                j++;
+        int total = 0;
+        int maxTime = neededTime[0];
+        
+        for(int i = 1; i < colors.size(); i++) {
+            if(colors[i] == colors[i - 1]) {
+                total += min(maxTime, neededTime[i]);
+                maxTime = max(maxTime, neededTime[i]);
+            } else {
+                maxTime = neededTime[i];
             }
-            if (j > i) { // group found
-                subarrays.emplace_back(i, j);
-            }
-            i = j + 1;
         }
-
-        vector<int> prefixSum(n + 1, 0);
-        for (int k = 0; k < n; k++) {
-            prefixSum[k + 1] = prefixSum[k] + neededTime[k];
-        }
-
-        int ans = 0;
-        for (auto& p : subarrays) {
-            ans += process(p, prefixSum, neededTime);
-        }
-
-        return ans;
+        return total;
     }
 };
