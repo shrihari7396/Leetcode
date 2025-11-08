10fba8dad3e085284875f97c2b02b8d04c084535
SHRIHARI KULKARNI <163467964+shrihari7396@users.noreply.github.com>
2025-11-08 16:12:33 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (9.57%) - LeetSync

--- 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
+++ 114-flatten-binary-tree-to-linked-list/flatten-binary-tree-to-linked-list.cpp
@@ -1,22 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 private:
     void preorder(TreeNode* root, vector<int>& ans) {
-        if (!root) return;
-        ans.push_back(root->val);
-        preorder(root->left, ans);
-        preorder(root->right, ans);
+        if(!root) return;
+        ans.emplace_back(root -> val);
+        preorder(root -> left, ans);
+        preorder(root -> right, ans);
     }
-
 public:
     void flatten(TreeNode* root) {
-        if (!root) return;
+        if(!root) return;
         vector<int> pre;
         preorder(root, pre);
         TreeNode* curr = root;
-        for (int i = 1; i < pre.size(); i++) {
-            curr->left = nullptr;
-            curr->right = new TreeNode(pre[i]);
-            curr = curr->right;
+        root -> left = nullptr;
+        for(int i = 1; i < pre.size(); i++) {
+            TreeNode* temp = new TreeNode(pre[i]);
+            curr -> right = temp;
+            curr = curr -> right;
         }
     }
 };
