78f4c122949b4ac154bb80d08548ba0899ea8141
SHRIHARI KULKARNI <163467964+shrihari7396@users.noreply.github.com>
2025-11-22 16:57:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (76.15%) - LeetSync

---
+++ 2177-check-whether-two-strings-are-almost-equivalent/check-whether-two-strings-are-almost-equivalent.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool checkAlmostEquivalent(string word1, string word2) {
+        vector<int> freq1(26,0), freq2(26, 0);
+        int n = word2.size();
+        for(int i = 0; i < n; i++) {
+            freq1[word1[i]-'a']++;
+            freq2[word2[i]-'a']++;
+        }
+
+        int diff = 0;
+        for(int i = 0; i < 26; i++) {
+            if(abs(freq1[i]-freq2[i]) > 3) return false;
+        }
+        return true;
+    }
+};
