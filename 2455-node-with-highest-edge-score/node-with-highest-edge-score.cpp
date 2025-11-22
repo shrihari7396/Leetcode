0f7b367edc522978874b1c542d25b4824c7cb8dd
SHRIHARI KULKARNI <163467964+shrihari7396@users.noreply.github.com>
2025-11-22 16:42:35 +0530

    Time: 0 ms (100.00%) | Memory: 124.2 MB (47.55%) - LeetSync

--- 2455-node-with-highest-edge-score/node-with-highest-edge-score.cpp
+++ 2455-node-with-highest-edge-score/node-with-highest-edge-score.cpp
@@ -1,26 +1,24 @@
 class Solution {
-private:
-    int solve(int n, vector<int>& edges) {
-        vector<long long> inDegrees(n, 0);
-        long long maxi = 0;
-
-        for (int i = 0; i < n; i++) {
-            int v = edges[i];
-            inDegrees[v] += i;
-            maxi = max(maxi, inDegrees[v]);
-        }
-
-        for (int it = 0; it < n; it++) {
-            if (inDegrees[it] == maxi) {
-                return it;
-            }
-        }
-        return -1; // fix
-    }
-
 public:
     int edgeScore(vector<int>& edges) {
         int n = edges.size();
-        return solve(n, edges);
+        vector<long long> score(n, 0);
+
+        long long maxi = 0;
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            int v = edges[i];
+            score[v] += i;
+
+            if (score[v] > maxi) {
+                maxi = score[v];
+                ans = v;
+            }
+            else if (score[v] == maxi && v < ans) {
+                ans = v;
+            }
+        }
+        return ans;
     }
 };
