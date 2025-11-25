6087a8b12f6e92025a3e459944bda5b29aa7b7df
SHRIHARI KULKARNI <163467964+shrihari7396@users.noreply.github.com>
2025-11-25 10:02:39 +0530

    Time: 166 ms (38.33%) | Memory: 86.4 MB (57.67%) - LeetSync

--- 2718-minimum-operations-to-make-all-array-elements-equal/minimum-operations-to-make-all-array-elements-equal.cpp
+++ 2718-minimum-operations-to-make-all-array-elements-equal/minimum-operations-to-make-all-array-elements-equal.cpp
@@ -1,31 +1,38 @@
 class Solution {
+private:
+    long long f(vector<int>& nums, int q) {
+        long long ans = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            ans += abs(nums[i]-q);
+        }
+        return ans;
+    }
 public:
     vector<long long> minOperations(vector<int>& nums, vector<int>& queries) {
+        vector<long long> ans;
         sort(nums.begin(), nums.end());
         int n = nums.size();
-
-        vector<long long> prefix(n);
-        prefix[0] = nums[0];
-        for(int i = 1; i < n; i++)
-            prefix[i] = prefix[i-1] + nums[i];
-
-        vector<long long> ans;
-
-        for (int q : queries) {
-            int k = upper_bound(nums.begin(), nums.end(), q) - nums.begin();
-
-            long long left = 0;
-            if (k > 0)
-                left = 1LL*q*k - prefix[k-1];
-
-            long long right = 0;
-            if (k < n)
-                right = (prefix[n-1] - (k > 0 ? prefix[k-1] : 0))
-                        - 1LL*q*(n-k);
-
-            ans.push_back(left + right);
+        vector<long long> prefixSum(n,0);
+        prefixSum[0] = nums[0];
+        
+        for(int i = 1; i < n; i++) {
+            prefixSum[i] = prefixSum[i-1]+nums[i];
         }
 
+        for(auto& it : queries) {
+            int k = upper_bound(nums.begin(), nums.end(), it) - nums.begin();
+            long long left = 0, right = 0;
+            if(k > 0) {
+                left = 1LL * k * it - prefixSum[k-1];
+            }
+
+            if(k < n) {
+                right = (prefixSum[n-1] - (k > 0 ? prefixSum[k-1] : 0))
+                        - 1LL*it*(n-k);
+            }
+
+            ans.emplace_back(left + right);
+        }
         return ans;
     }
 };
