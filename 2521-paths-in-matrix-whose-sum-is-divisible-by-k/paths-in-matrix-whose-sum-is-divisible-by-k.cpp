349b7af6795bb7ca88053bf2e484ec8cfb4bb111
SHRIHARI KULKARNI <163467964+shrihari7396@users.noreply.github.com>
2025-11-26 13:50:08 +0530

    Time: 269 ms (51.89%) | Memory: 139.9 MB (37.68%) - LeetSync

--- 2521-paths-in-matrix-whose-sum-is-divisible-by-k/paths-in-matrix-whose-sum-is-divisible-by-k.cpp
+++ 2521-paths-in-matrix-whose-sum-is-divisible-by-k/paths-in-matrix-whose-sum-is-divisible-by-k.cpp
@@ -1,30 +1,38 @@
 class Solution {
+private:
+    const int MOD = 1e9 + 7;
+    vector<vector<vector<int>>> dp;
+    int f(int i, int j, vector<vector<int>>& grid, int k, int m, int n,
+          int rem) {
+        if (i > m - 1 || j > n - 1)
+            return 0;
+
+        int newRem = (rem + grid[i][j]) % k;
+
+        if (i == m - 1 && j == n - 1) {
+            return (newRem == 0) ? 1 : 0;
+        }
+
+        if (dp[i][j][newRem] != -1)
+            return dp[i][j][newRem];
+
+        long long ways = 0;
+
+        ways += f(i + 1, j, grid, k, m, n, newRem);
+        ways %= MOD;
+
+        ways += f(i, j + 1, grid, k, m, n, newRem);
+        ways %= MOD;
+
+        return dp[i][j][newRem] = ways;
+        ;
+    }
+
 public:
     int numberOfPaths(vector<vector<int>>& grid, int k) {
         int m = grid.size();
         int n = grid[0].size();
-        const int MOD = 1e9 + 7;
-
-        vector<vector<vector<int>>> dp(m, vector<vector<int>>(n, vector<int>(k, 0)));
-
-        dp[0][0][grid[0][0] % k] = 1;
-
-        for(int i = 0; i < m; i++) {
-            for(int j = 0; j < n; j++) {
-                for(int mod = 0; mod < k; mod++) {
-
-                    if(i > 0) {
-                        int newMod = (mod + grid[i][j]) % k;
-                        dp[i][j][newMod] = (dp[i][j][newMod] + dp[i-1][j][mod]) % MOD;
-                    }
-                    if(j > 0) {
-                        int newMod = (mod + grid[i][j]) % k;
-                        dp[i][j][newMod] = (dp[i][j][newMod] + dp[i][j-1][mod]) % MOD;
-                    }
-                }
-            }
-        }
-
-        return dp[m-1][n-1][0];
+        dp.assign(m, vector<vector<int>>(n, vector<int>(k, -1)));
+        return f(0, 0, grid, k, m, n, 0);
     }
 };
