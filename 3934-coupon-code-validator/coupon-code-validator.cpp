9d6dcf5f831df74247fe33577a715d022ce020c6
SHRIHARI KULKARNI <163467964+shrihari7396@users.noreply.github.com>
2025-12-14 01:25:25 +0530

    Time: 11 ms (30.26%) | Memory: 69.8 MB (31.58%) - LeetSync

---
+++ 3934-coupon-code-validator/coupon-code-validator.cpp
@@ -0,0 +1,56 @@
+class Solution {
+private:
+    bool first(const string& s) {
+        if (s.empty())
+            return false;
+
+        for (char it : s) {
+            if (!((it >= 'a' && it <= 'z') || (it >= 'A' && it <= 'Z') ||
+                  (it >= '0' && it <= '9') || // allow digits
+                  (it == '_'))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool second(const string& s) {
+        return s == "electronics" || s == "grocery" || s == "pharmacy" ||
+               s == "restaurant";
+    }
+
+public:
+    vector<string> validateCoupons(vector<string>& code,
+                                   vector<string>& businessLine,
+                                   vector<bool>& isActive) {
+
+        int n = code.size();
+
+        unordered_map<string, int> priority = {{"electronics", 0},
+                                               {"grocery", 1},
+                                               {"pharmacy", 2},
+                                               {"restaurant", 3}};
+
+        vector<pair<string, string>> valid;
+        for (int i = 0; i < n; i++) {
+            if (isActive[i] && first(code[i]) && second(businessLine[i])) {
+
+                valid.push_back({businessLine[i], code[i]});
+            }
+        }
+
+        sort(valid.begin(), valid.end(), [&](auto& a, auto& b) {
+            if (priority[a.first] != priority[b.first]) {
+                return priority[a.first] < priority[b.first];
+            }
+            return a.second < b.second;
+        });
+
+        vector<string> ans;
+        for (auto& p : valid) {
+            ans.push_back(p.second);
+        }
+
+        return ans;
+    }
+};
